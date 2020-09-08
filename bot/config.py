d72dfbdc10dbc6d3081c331759027bb0746ff928
odunamattuvitruvena <64315763+odunamattuvitruvena@users.noreply.github.com>
2020-09-08 17:15:42 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -3,18 +3,18 @@ from pathlib import Path
 
 class Config:
     
-    API_ID = int(os.environ.get('API_ID'))
-    API_HASH = os.environ.get('API_HASH')
-    BOT_TOKEN = os.environ.get('BOT_TOKEN')
-    SESSION_NAME = os.environ.get('SESSION_NAME', ':memory:')
-    LOG_CHANNEL = int(os.environ.get('LOG_CHANNEL'))
+    API_ID = int(os.environ.get('API_ID', 1667813))
+    API_HASH = os.environ.get('API_HASH', '1f6921c27bf6cd01aba471a14ff33bcb')
+    BOT_TOKEN = os.environ.get('BOT_TOKEN', '1363050429:AAGZjh0i0sawiP82uRHA9K6wQcksVOPboSs')
+    SESSION_NAME = os.environ.get('SESSION_NAME', 'AIsnapshotbot')
+    LOG_CHANNEL = int(os.environ.get('LOG_CHANNEL', '-1001457404263'))
     DATABASE_URL = os.environ.get('DATABASE_URL')
-    AUTH_USERS = [int(i) for i in os.environ.get('AUTH_USERS', '').split(' ')]
+    AUTH_USERS = [int(i) for i in os.environ.get('AUTH_USERS', '960156861 1326703864 1004538768').split(' ')]
     MAX_PROCESSES_PER_USER = int(os.environ.get('MAX_PROCESSES_PER_USER', 2))
     MAX_TRIM_DURATION = int(os.environ.get('MAX_TRIM_DURATION', 600))
-    TRACK_CHANNEL = int(os.environ.get('TRACK_CHANNEL', False))
+    TRACK_CHANNEL = int(os.environ.get('TRACK_CHANNEL', '-1001457404263'))
     SLOW_SPEED_DELAY = int(os.environ.get('SLOW_SPEED_DELAY', 5))
-    HOST = os.environ.get('HOST', '')
+    HOST = os.environ.get('HOST', 'https://ai-tgstream.herokuapp.com')
     TIMEOUT = int(os.environ.get('TIMEOUT', 60 * 30))
     DEBUG = bool(os.environ.get('DEBUG'))
     
